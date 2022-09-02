2e8c12ebd826d545aae0d780700419b3402c2086
shravkv <sravan.k.vodnala@gmail.com>
2022-09-02 08:25:31 +0530

    Added: Extend the servlet to accept a valid name

--- src/main/java/bridgelabz/LoginServlet.java
+++ src/main/java/bridgelabz/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet testing",
@@ -27,13 +28,13 @@ public class LoginServlet extends HttpServlet {
         String pwd = request.getParameter("pwd");
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if (userId.equals(user) && password.equals(pwd)) {
+        if (Pattern.matches("[A-Z][a-z]{2,}", userId) && (userId.equals(user) && password.equals(pwd))) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color = red > Either user name or password is wrong.</font>");
+            out.println("<font color = red>Either username or password are wrong.<font>");
             rd.include(request, response);
         }
     }
